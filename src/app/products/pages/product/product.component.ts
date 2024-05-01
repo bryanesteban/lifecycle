4cc64faa288d5833edf4165ec328f693244c04ca
bryanesteban <bryanesteban_123@hotmail.com>
2024-05-01 16:08:14 -0500

    cyclo of life

--- src/app/products/pages/product/product.component.ts
+++ src/app/products/pages/product/product.component.ts
@@ -7,33 +7,41 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 })
 export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
 
+  public isProductVisible:boolean = false;
+  public currentPrice: number = 10;
+
   constructor(){
     console.log("Constructor")
   }
 
   ngOnInit(): void {
-    console.log('ngOnInit')
+    console.log('ngOnInit');
   }
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('ngOnChanges')
+    console.log({changes});
+    console.log('SimpleChanges');
   }
   ngDoCheck(): void {
-    console.log('ngDoCheck')
+    console.log('ngDoCheck');
   }
   ngAfterContentInit(): void {
-    console.log('ngAfterContentInit')
+    console.log('ngAfterContentInit');
   }
   ngAfterContentChecked(): void {
-    console.log('ngAfterContentChecked')
+    console.log('ngAfterContentChecked');
   }
   ngAfterViewInit(): void {
-    console.log('ngAfterViewInit')
+    console.log('ngAfterViewInit');
   }
   ngAfterViewChecked(): void {
-    console.log('ngAfterViewChecked')
+    console.log('ngAfterViewChecked');
   }
   ngOnDestroy(): void {
-    console.log('ngOnDestroy')
+    console.log('ngOnDestroy');
+  }
+
+  increasePrice(){
+    this.currentPrice++;
   }
 
 }
